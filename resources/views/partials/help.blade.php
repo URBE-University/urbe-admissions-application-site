7468b5662ac96d61cad60bce63ed3c8f746b091b
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-01-08 13:39:34 -0500

    ..

--- resources/views/partials/help.blade.php
+++ resources/views/partials/help.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-white rounded-md shadow z-auto p-4 text-sm mb-2" x-show="helpMenu" @click.outside="helpMenu = ! helpMenu" x-cloak>
         <ul class="text-left flex flex-col">
             <li>
-                <a class="inline-flex items-center gap-2 p-2 rounded hover:bg-sky-50 group hover:text-sky-600 transition-all duration-300" href="href:admissions@urbe.university">
+                <a class="inline-flex items-center gap-2 p-2 rounded hover:bg-sky-50 group hover:text-sky-600 transition-all duration-300" href="mailto:admissions@urbe.university?subject=Question+about+admissions+at+URBE">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 group-hover:-rotate-45 transition-all duration-300">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                     </svg>
