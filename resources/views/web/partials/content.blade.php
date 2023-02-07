201d0b8d1a1505dad2e5d7483a4af55f87342202
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-02-07 12:47:44 -0500

    ..

--- resources/views/web/partials/content.blade.php
+++ resources/views/web/partials/content.blade.php
@@ -13,13 +13,13 @@
                     <p class="mt-3 text-lg">{{__("Before you start your application, please take a moment to review your program-specific requirements.")}}</p>
                 </div>
                 <div class="mt-6 sm:flex items-center gap-8 text-lg">
-                    <a href="{{ asset('Entrance-Requirements-Bachelors.pdf') }}" target="_blank" class="inline-flex items-center gap-2 text-base text-sky-600 hover:-translate-y-1 transition-transform">
+                    <a href="{{ asset('Entrance-Requirements-Bachelors.pdf') }}" target="_blank" class="inline-flex items-center gap-2 text-base text-sky-800 hover:-translate-y-1 transition-transform">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M2 9.5A3.5 3.5 0 005.5 13H9v2.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 15.586V13h2.5a4.5 4.5 0 10-.616-8.958 4.002 4.002 0 10-7.753 1.977A3.5 3.5 0 002 9.5zm9 3.5H9V8a1 1 0 012 0v5z" clip-rule="evenodd" />
                         </svg>
                         <span>{{__("Bachelor's requirements")}}</span>
                     </a>
-                    <a href="{{ asset('Entrance-Requirements-Masters.pdf') }}" target="_blank" class="inline-flex items-center gap-2 text-base text-sky-600 hover:-translate-y-1 transition-transform">
+                    <a href="{{ asset('Entrance-Requirements-Masters.pdf') }}" target="_blank" class="inline-flex items-center gap-2 text-base text-sky-800 hover:-translate-y-1 transition-transform">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M2 9.5A3.5 3.5 0 005.5 13H9v2.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 15.586V13h2.5a4.5 4.5 0 10-.616-8.958 4.002 4.002 0 10-7.753 1.977A3.5 3.5 0 002 9.5zm9 3.5H9V8a1 1 0 012 0v5z" clip-rule="evenodd" />
                         </svg>
