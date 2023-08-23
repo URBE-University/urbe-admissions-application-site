f06d1c3eb8d8db0dd5d997e17cc6f27e569d15ac
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-08-23 13:28:53 -0400

    ..

--- resources/views/vendor/jetstream/components/button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-[#073260] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-[#34577d] active:bg-[#073260] focus:outline-none focus:border-[#073260] focus:ring focus:ring-gray-300 disabled:opacity-25 transition']) }}>
-    {{ $slot }}
-</button>
