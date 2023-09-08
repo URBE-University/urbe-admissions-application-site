d77d5ba70f1ec29b9419d2c4ca74eaef999a5340
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-09-08 11:41:05 -0400

    ..

--- app/Http/Livewire/Admin/Applications/Index.php
+++ app/Http/Livewire/Admin/Applications/Index.php
@@ -15,7 +15,7 @@ class Index extends Component
     public function render()
     {
         return view('livewire.admin.applications.index', [
-            'applications' => Application::where('last_name', '%' . $this->search . '%')->paginate(25)
+            'applications' => Application::paginate(25)
         ]);
     }
 }
